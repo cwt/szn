2e914123ac6b406541db42e021abf7bf48eb3b24
Chaiwat Suttipongsakul <cwt@bashell.com>
2026-06-23 14:42:57 +0700

    Enable ThinLTO for non-debug builds on non-Darwin targets

--- build.zig
+++ build.zig
@@ -15,6 +15,11 @@ pub fn build(b: *std.Build) void {
         .name = "szn",
         .root_module = exe_module,
     });
+    const is_darwin = target.result.os.tag.isDarwin();
+    if (optimize != .Debug and !is_darwin) {
+        exe.lto = .thin;
+        exe.use_lld = true;
+    }
     b.installArtifact(exe);
 
     const run_cmd = b.addRunArtifact(exe);
@@ -32,6 +37,10 @@ pub fn build(b: *std.Build) void {
     const test_exe = b.addTest(.{
         .root_module = test_module,
     });
+    if (optimize != .Debug and !is_darwin) {
+        test_exe.lto = .thin;
+        test_exe.use_lld = true;
+    }
     const test_run = b.addRunArtifact(test_exe);
     const test_step = b.step("test", "Run all tests");
     test_step.dependOn(&test_run.step);
